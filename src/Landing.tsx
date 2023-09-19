af2287b37aa81e6995559789b33e4b1b88d7d0ce
Calli Herrmann <calliherrmann@gmail.com>
2023-09-19 15:53:00 -0400

    Add components

---
+++ src/Landing.tsx
@@ -0,0 +1,11 @@
+
+const Landing = () => {
+  return (
+    <div>
+      <h1>News Reader</h1>
+      {/* Top Articles - like 3?*/}
+    </div>
+  )
+}
+
+export default Landing
---
+++ src/Nav.tsx
@@ -0,0 +1,10 @@
+
+const Nav = () => {
+  return (
+    <div className="w-screen h-4 bg-blue-700">
+      <input placeholder="Search for an article"></input>
+    </div>
+  )
+}
+
+export default Nav
